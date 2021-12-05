318ecd4c39551f1a640a8b6e759051c78ee18ba5
rajeshdevika <rajeshdvk1995@gmail.com>
2021-12-05 16:32:52 +0530

    y

---
+++ assignment10.py
@@ -0,0 +1,6 @@
+a=input("enter: ").split()
+print(a)
+c=input('enter: ')
+for i in range(len(a)):
+    if(i[0]==c):
+        print(i)
---
+++ assignment9.py
@@ -0,0 +1,7 @@
+a=int(input('enter: '))
+s=[]
+for i in range(a):
+    b=int(input(""))
+    s.append(b)
+c=list(sorted(s))
+print(s[-2])
