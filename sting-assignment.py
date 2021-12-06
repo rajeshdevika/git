2b1ae5939a0b6ca48c664d73b29383bc9ed158d0
rajeshdevika <rajeshdvk1995@gmail.com>
2021-12-06 18:34:52 +0530

    first commit

--- sting-assignment.py
+++ sting-assignment.py
@@ -1,8 +1,4 @@
 string = "Python is awesome, isn't it?"
 substring = "i"
-
-# count after first 'i' and before the last 'i'
 count = string.count("isn")
-
-# print count
 print("The count is:", count)
